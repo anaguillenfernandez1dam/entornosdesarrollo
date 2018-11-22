3164e6affc1a99bf771e88309ce3be6a457f54ed
anaguillenfernandez1dam <1dam.aguillen@institutoserpis.org>
2018-11-22 11:26:55 +0100

    Añadidos carpeta de proyectos MonoDevelop con HolaMundo y DimeNombre y modificado diario.txt

---
+++ ProyectosMonoDevelop/Proyectos/HolaMundo/DimeNombre.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace HolaMundo
+{
+    public class DimeNombre
+    {
+        public static void Main(string[] args){
+
+            string nombre;
+            Console.WriteLine("Dime tu nombre");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("Tu nombre es " + nombre);
+        }
+       
+    }
+}
