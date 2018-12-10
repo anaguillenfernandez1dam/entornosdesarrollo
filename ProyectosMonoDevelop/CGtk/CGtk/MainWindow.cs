060595e5f23b11d35670d7906cc7cb9314ee7f87
anaguillenfernandez1dam <1dam.aguillen@institutoserpis.org>
2018-12-10 09:12:37 +0100

    Añadido proyecto CGtk para practicar con Gtk

---
+++ ProyectosMonoDevelop/CGtk/CGtk/MainWindow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+
+        Button button = new Button();
+        button.Visible = true;
+        butt
+        vBox.Add(button);
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+}
---
+++ ProyectosMonoDevelop/Calculadora/Calculadora/Calculadora/MainWindow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+        buttonSuma.Clicked += delegate
+        {
+            //int num1 = Int32.Parse(entry1.Text);
+            //int num2 = Int32.Parse(entry2.Text);
+            //int resultado = num1 + num2;
+            //labelResultado.Text = resultado.ToString();    
+        };
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+}
