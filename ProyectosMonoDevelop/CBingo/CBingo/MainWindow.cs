a93fa5afcc8a5039ef2c815f3c5b7a17ea0d95d8
anaguillenfernandez1dam <1dam.aguillen@institutoserpis.org>
2019-01-10 12:21:10 +0100

    Añadidas clases Panel y Bombo

--- ProyectosMonoDevelop/CBingo/CBingo/MainWindow.cs
+++ ProyectosMonoDevelop/CBingo/CBingo/MainWindow.cs
@@ -3,50 +3,57 @@ using Gtk;
 using System;
 using System.Collections.Generic;
 
+using CBingo;
+
 public partial class MainWindow : Gtk.Window
 {
     private static readonly Gdk.Color COLOR_GREEN = new Gdk.Color(0, 255, 0);
     public MainWindow() : base(Gtk.WindowType.Toplevel)
     {
         Build();
-        //Button[] buttons = new Button[90]
-        Random random = new Random();
-        List<int> numeros = new List<int>();
-        numeros = rellenarLista(numeros);
-        List<Button> buttons = new List<Button>();
 
-        for (int i = 1; i < 91; i++){
-            Button button = new Button();
-            button.Label = i.ToString();
-            buttons.Add(button);
-        }
+        Panel panel = new Panel(vbox);
+        Bombo bombo = new Bombo();
 
-        int cont = 90;
-        for (int row = 0; row < 5; row++)
-            for (int column = 0; column < 7; column++)
-            {
-                int numBoton = random.Next(cont);
-                Button button = buttons[numBoton];
-                numeros.Remove(numeros[numBoton]);
-                cont--;
-
-                table.Attach(button, (uint)column, (uint)column + 1, (uint)row, (uint)row+1);
-                buttons.Remove(buttons[numBoton]);
-            }
-
-        table.ShowAll();
-
-        cont = 90;
-        buttonSiguiente.Clicked += delegate {
-
-            int numAleatorio = numeros[random.Next(cont)];
-            labelNumero.Text = numAleatorio.ToString();
-
-            buttons[numAleatorio].ModifyBg(StateType.Normal, COLOR_GREEN);
-            numeros.Remove(numeros[numAleatorio]);
-            cont--;
-
+        buttonSiguiente.Clicked += delegate
+        {
+            panel.Marcar(32);
+            int numero = bombo.sacarBola();
         };
+        ////Button[] buttons = new Button[90]
+        //Random random = new Random();
+        //List<int> numeros = new List<int>();
+        //numeros = rellenarLista(numeros);
+        //List<Button> buttons = new List<Button>();
+
+        //for (int i = 1; i < 91; i++){
+        //    Button button = new Button();
+        //    button.Label = i.ToString();
+        //    buttons.Add(button);
+        //}
+
+        //int cont = 0;
+        //for (int row = 0; row < 9; row++)
+        //    for (int column = 0; column < 10; column++)
+        //    {
+        //        Button button = buttons[cont];
+        //        cont++;
+        //        table.Attach(button, (uint)column, (uint)column + 1, (uint)row, (uint)row+1);
+        //    }
+
+        //table.ShowAll();
+
+        //cont = 90;
+        //buttonSiguiente.Clicked += delegate {
+
+        //    int numAleatorio = random.Next(cont)-1;
+        //    labelNumero.Text = numeros[numAleatorio].ToString();
+
+        //    buttons[numeros[numAleatorio]].ModifyBg(StateType.Normal, COLOR_GREEN);
+        //    numeros.Remove(numeros[numAleatorio]);
+        //    cont--;
+
+        //};
     }
 
     public List<int> rellenarLista(List<int> numeros){
