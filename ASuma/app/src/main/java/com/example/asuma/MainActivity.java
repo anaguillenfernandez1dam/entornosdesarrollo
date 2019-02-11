bba26f15b1b96fa501159e240442ae645ad66919
anaguillenfernandez1dam <1dam.aguillen@institutoserpis.org>
2019-02-11 09:56:54 +0100

    Añadido metodo resta

--- ASuma/app/src/main/java/com/example/asuma/MainActivity.java
+++ ASuma/app/src/main/java/com/example/asuma/MainActivity.java
@@ -6,23 +6,48 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.math.BigDecimal;
+
 public class MainActivity extends AppCompatActivity {
+
+    private EditText num1;
+    private EditText num2;
+    private TextView textoResult;
+    private BigDecimal resultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        num1 = findViewById(R.id.num1);
+        num2 = findViewById(R.id.num2);
+        textoResult = findViewById(R.id.resultado);
+    }
+
+    private BigDecimal getNum1(){
+        String string1 = num1.getText().toString();
+        return new BigDecimal(string1);
+    }
+
+    private BigDecimal getNum2(){
+        String string2 = num2.getText().toString();
+        return new BigDecimal(string2);
     }
 
     public void sumar(View view){
 
-        EditText num1 = findViewById(R.id.num1);
-        EditText num2 = findViewById(R.id.num2);
+        resultado = getNum1().add(getNum2());
+        textoResult.setText("Resultado: " + String.valueOf(resultado));
 
-        int resultado = Integer.parseInt(num1.getText().toString()) + Integer.parseInt(num2.getText().toString());
+    }
 
-        TextView textoResult = findViewById(R.id.resultado);
-        textoResult.setText("Resultado: " + Integer.toString(resultado));
+    public void restar(View view){
+
+        resultado = getNum1().subtract(getNum2());
+        textoResult.setText("Resultado: " + String.valueOf(resultado));
+
+
 
     }
 
