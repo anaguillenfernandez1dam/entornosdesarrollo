bd1885a4b30d7ce8a20eeead5c0e7e1e2af64de8
anaguillenfernandez1dam <1dam.aguillen@institutoserpis.org>
2019-03-11 10:40:28 +0100

    Actualizado APares con métodos postDelayed y leafChildren

--- APares/app/src/main/java/com/example/apares/MainActivity.java
+++ APares/app/src/main/java/com/example/apares/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.apares;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -17,14 +19,13 @@ public class MainActivity extends AppCompatActivity {
     List<String> palabras = new ArrayList<>();
     private String frase = "Lobo Rana Oveja Gato Pato Llama Lobo Rana Oveja Gato Pato Llama";
     private String texto1, texto2;
-
+    private Button boton1, boton2;
     private TextView texto;
-
+    private ConstraintLayout layout2;
     private boolean flag;
-
-    List<Button> buttons = new ArrayList<>();
-
+    List<Button> buttons;
     private int aciertos;
+
 
     private void initPalabras(){
         palabras.addAll(Arrays.asList(frase.split(" ")));
@@ -33,50 +34,83 @@ public class MainActivity extends AppCompatActivity {
 
     private void initButtons(){
 
-        buttons.add((Button) findViewById(R.id.t1));
-        buttons.add((Button) findViewById(R.id.t2));
-        buttons.add((Button) findViewById(R.id.t3));
-        buttons.add((Button) findViewById(R.id.t4));
-        buttons.add((Button) findViewById(R.id.t5));
-        buttons.add((Button) findViewById(R.id.t6));
-        buttons.add((Button) findViewById(R.id.t7));
-        buttons.add((Button) findViewById(R.id.t8));
-        buttons.add((Button) findViewById(R.id.t9));
-        buttons.add((Button) findViewById(R.id.t10));
-        buttons.add((Button) findViewById(R.id.t11));
-        buttons.add((Button) findViewById(R.id.t12));
+        buttons = getLeafButtons(layout2);
 
         for (int i = 0; i < buttons.size(); i++){
             buttons.get(i).setText(palabras.get(i));
         }
+
+        layout2.postDelayed( new Runnable(){
+            @Override
+            public void run(){
+                for (int i = 0; i < buttons.size(); i++){
+                    buttons.get(i).setText("");
+                    buttons.get(i).setTag(palabras.get(i));
+                }
+            }
+        }, 2000);
+
+    }
+
+    private List<Button> getLeafButtons(ViewGroup viewGroup){
+        List<Button> leafChildren = new ArrayList<>();
+
+        for (int i= 0; i < viewGroup.getChildCount(); i++){
+            Button boton = (Button) viewGroup.getChildAt(i);
+            leafChildren.add(boton);
+        }
+
+        return leafChildren;
     }
 
     public void tarjetaOnClick(View view){
 
-        //Button boton = (Button) findViewById(view.getId());
-        Button boton = (Button)view;
-        int index = buttons.indexOf(boton);
+        if (view.getTag() != ""){
+            //Button boton = (Button) findViewById(view.getId());
+            if (!flag){
+                boton1 = (Button) view;
+                texto1 = (String) boton1.getTag();
+                boton1.setText((String)boton1.getTag());
+                flag = true;
 
-        if (!flag){
-            texto1 = (String) boton.getText();
-            flag = true;
-        }else{
-            texto2 = (String) boton.getText();
-            flag = false;
+            }else{
+                boton2 = (Button) view;
+                texto2 = (String) boton2.getTag();
+                boton2.setText((String)boton2.getTag());
 
-            if (texto1.equals(texto2)){
-                aciertos++;
-                texto.setText("Aciertos: " + aciertos);
-                texto1 = null;
-                texto2 = null;
-            }
-        }
+                flag = false;
+
+                if (texto1.equals(texto2)) {
+                    aciertos++;
+                    texto.setText("Aciertos: " + aciertos);
+                    texto1 = null;
+                    texto2 = null;
+                    boton1.setTag("");
+                    boton2.setTag("");
+                }else{
+                    boton1.postDelayed(new Runnable(){
+                        @Override
+                        public void run(){
+                            boton1.setText("");
+                            boton2.setText("");
+                        }
+                    }, 1000);
+                }
+            }//Fin comprobar flag
+        }//Fin comprobar tag
+
 
         if (aciertos >= 6){
             texto.setText("¡Felicidades, has ganado!");
         }
 
+    }//fin tarjetaOnClick
 
+    public void reiniciar(View view){
+        aciertos = 0;
+        texto.setText("Aciertos: " + aciertos);
+        Collections.shuffle(palabras);
+        initButtons();
     }
 
 
@@ -86,12 +120,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         texto = findViewById(R.id.textoIntentos);
+        layout2 = findViewById(R.id.ConstraintLayout2);
         initPalabras();
         initButtons();
 
         aciertos = 0;
         flag = false;
-
 
         texto.setText("Aciertos: " + aciertos);
 
